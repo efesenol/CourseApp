df160c4a1ba472602f8b1e63f48d121cc38ace2f
efesenol <efeesenol4471@gmail.com>
2025-02-08 18:47:48 +0300

    TagHelpers Add
    TagHelpers eklendi

--- obj/Debug/net7.0/CourseApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/CourseApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d8fd12e2c7134da542dd73a7daa877e21578656")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
